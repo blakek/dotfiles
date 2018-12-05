01fb544b517ca06382e1c350f33b9e891b394bf5
Blake Knight <bknight@gsandf.com>
2018-12-05 10:57:22 -0600

    :pencil2: adjust whitespace to match other scripts

--- prompts/bkPrompt.sh
+++ prompts/bkPrompt.sh
@@ -1,14 +1,13 @@
 prompt() {
-  local lastReturn="$?";
-  local prompt="${primaryColor}[\W${repoColor}\$(__git_ps1 \" %s\")${primaryColor}]";
+	local lastReturn="$?"
+	local prompt="${primaryColor}[\W${repoColor}\$(__git_ps1 \" %s\")${primaryColor}]"
 
-  if [ $lastReturn == "0" ]; then
-    dolla=$primaryColor"\$";
-  else
-    dolla=$errorColor"\$";
-  fi
+	if [ $lastReturn == "0" ]; then
+		dolla=$primaryColor"\$"
+	else
+		dolla=$errorColor"\$"
+	fi
 
-  PS1="${prompt}${dolla} ${e_reset}"
-
-  update_terminal_cwd 2>/dev/null
+	PS1="${prompt}${dolla} ${e_reset}"
+	update_terminal_cwd 2>/dev/null
 }
