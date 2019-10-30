31b37dad8b77f56998a0ad047b1621f06bf86c67
Blake Knight <bknight@gsandf.com>
2019-10-30 08:16:25 -0500

    :sparkles: add script for dumping installed applications; add initial npm dump

---
+++ get-installed-files.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+__dirname="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# Save applications installed with Homebrew
+brew bundle dump --all --describe --force
+
+# Save applications installed with npm
+npm ls -g --depth=0 --json \
+  | fx 'Object.keys(this.dependencies).filter(d => d !== "npm")' \
+  > npm-global.json
