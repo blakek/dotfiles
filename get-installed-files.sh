679cbcbb6c6f80835dfe06ca6eb94579feba5201
Blake Knight <oss.ideas@gmail.com>
2021-07-23 21:41:28 -0500

    :art: simplify getting globally installed npm modules

--- get-installed-files.sh
+++ get-installed-files.sh
@@ -5,5 +5,5 @@ brew bundle dump --all --describe --force
 
 # Save applications installed with npm
 npm ls -g --depth=0 --json |
-	fx 'Object.keys(this.dependencies).filter(d => d !== "npm")' \
+	jq '.dependencies | keys | . - ["npm"]' \
 		>npm-global.json
