5acf13d2a4b5a2233276a42ca51378e03aef8825
Blake Knight <oss.ideas@gmail.com>
2025-06-04 11:25:46 -0500

    🧹 Clean up shell options in bash profile
    - Removed unused shell options for clarity
    - Simplified shopt command for better readability

--- .bash_profile_extensions/90-bash.bash
+++ .bash_profile_extensions/90-bash.bash
@@ -8,14 +8,11 @@ export EDITOR='vim'
 set -o emacs
 
 # Shell options
-#   autocd        `cd` into directories without typing `cd` (Bash 4+)
-#   cdspell       `cd` into directories if a minor typo is made
 #   histappend    append to - rather than overwriting - history
 #   hostcomplete  try completing hostnames when an `@` is found
 #   lithist       save long commands in history with newlines (not semicolons)
 #   no_empty_cmd_completion    don't try to complete an empty line
-shopt -s cdspell histappend hostcomplete lithist no_empty_cmd_completion
-[[ ${BASH_VERSINFO[0]} -ge 4 ]] && shopt -s autocd
+shopt -s histappend hostcomplete lithist no_empty_cmd_completion
 
 # Don't remember duplicate commands or commands with a leading space space.
 export HISTCONTROL='ignoreboth'
