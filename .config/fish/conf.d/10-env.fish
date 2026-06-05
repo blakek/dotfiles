072574747ae814a182dfd1622c5034c87a3188b4
Blake Knight <oss.ideas@gmail.com>
2026-06-05 16:29:43 -0500

    🎨 Enhance shell defaults for improved usability
    - Set EDITOR to vim and VISUAL to "code --wait"
    - Enable CLICOLOR for colored output
    - Configure PAGER to use custom bkpager if available

--- .config/fish/conf.d/10-env.fish
+++ .config/fish/conf.d/10-env.fish
@@ -1,3 +1,8 @@
+# Shell defaults
+set -gx EDITOR vim
+set -gx VISUAL "code --wait"
+set -gx CLICOLOR 1
+
 set -l current_file (path resolve (status -f))
 set -l current_dir (path dirname "$current_file")
 set -l dotfiles_root (path normalize -- "$current_dir/../../..")
@@ -5,6 +10,11 @@ set -l dotfiles_root (path normalize -- "$current_dir/../../..")
 # For this dotfiles repo…
 set -gx DOTFILES_ROOT "$dotfiles_root"
 set -gx DEV_ROOT "$HOME/dev"
+
+# Pager
+if test -x "$dotfiles_root/bin/bkpager"
+	set -gx PAGER "$dotfiles_root/bin/bkpager"
+end
 
 # Load ~/.env
 if test -f ~/.env
